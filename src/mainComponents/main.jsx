9427ed0dfbd3d9d3a437899663174922328cdc07
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-03-04 16:31:34 +0100

    added collections

--- src/mainComponents/main.jsx
+++ src/mainComponents/main.jsx
@@ -1,7 +1,26 @@
 import react,{ useState } from 'react'
 import '../mainComponentsCSS/main.css'
+import summer from '../collecions/summer.png'
+import next from '../imgs/next.png'
 
 function Main(){
+    return(
+        <main>
+            <div className='collections'>
+              <img
+                src={summer}
+              />
+              <p className='intro'>level up your style with our    <span>summer collection</span></p>
+              <button>shop now <img id='next' src={next} /></button>
+              <div className='dots'>
+                <p className='selected'>.</p>
+                <p>.</p>
+                <p>.</p>
+                <p>.</p>
+              </div>
+            </div>
+        </main>
+    )
 };
 
 export default Main;
