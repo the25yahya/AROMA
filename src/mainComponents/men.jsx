bd5dafe855c149d67bf8328237fa31ce966e17f8
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-03-08 16:43:00 +0100

    working on men section

--- src/mainComponents/men.jsx
+++ src/mainComponents/men.jsx
@@ -1,27 +1,44 @@
 import react,{ useState } from 'react'
 import '../mainComponentsCSS/men.css'
+import MenData from '../json-files/men.json';
 
 function Product(props){
     return(
         <div className='product'>
           <img
-            src={props.img}
+            src={props.img1}
           />
-          <h2>{props.description}</h2>
-          <p>{props.price}</p>
-          <p>{props.tag}</p>
+          <h2 id='name'>{props.name}</h2>
+          <p id='price'>{props.price}</p>
+          <p id='tag'>{props.tag}</p>
         </div>
     )
 }
 
 function Men(props){
+    const MenArray = MenData.map((item)=>{
+        return(
+          <Product 
+             key={item.id} 
+             img1={item.img1}
+             tag={item.tag}
+             name={item.name}
+             price={item.price}
+          />
+        )
+      })
     return(
-        <div className='navigation'>
-            <button>suits</button>
-            <button>jeans</button>
-            <button>sneakers</button>
-            <button>gym-wear</button>
-            <button>pants</button>
+        <div className='men'>
+           <div className='navigation'>
+              <button>suits</button>
+              <button>jeans</button>
+              <button>coats</button>
+              <button>waistcoats</button>
+              <button>pants</button>
+           </div>
+           <div className='products'>
+             {MenArray}
+           </div>
         </div>
     )
 }
