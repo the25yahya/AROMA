e78a64af71b9ceee2e06c3034cb1764b7edb68cf
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-03-08 23:22:22 +0100

    added phone dropdown menu

--- src/mainComponents/nav.jsx
+++ src/mainComponents/nav.jsx
@@ -5,9 +5,39 @@ import letter from '../imgs/letter-o.png'
 import user from '../imgs/user.png'
 import cart from '../imgs/cart.png'
 
+
+
+
+
+
+function DropDown(props){
+  const dropdownClassname = `dropdown ${props.isOpen ? 'open' : 'show'}`;
+  return(
+    <div className={dropdownClassname}>
+                <p onClick={props.onMainClick}>Home</p>
+                <p>new arrival</p>
+                <p onClick={props.onMenClick}>Men</p>
+                <p onClick={props.onWomenClick}>women</p>
+                <p>Casual</p>
+                <p>brands</p>
+    </div>
+  )
+}
+
+
 function Nav(props){
+  const [isDropDownOpen, setIsDropDownOpen] = useState(true);
+  const toggleDropDown = () => {
+    setIsDropDownOpen(prevState => !prevState);
+  };
   function reloadApp() {
     window.location.reload();
+  }
+  function toggleX() {
+    const bars = document.querySelectorAll('.bar');
+    bars.forEach((bar) => {
+      bar.classList.toggle('x');
+    });
   }
     return(
         <nav>
@@ -25,7 +55,8 @@ function Nav(props){
             <div className='features'>
                 <p onClick={props.onMainClick}>Home</p>
                 <p>new arrival</p>
-                <p onClick={props.onMenClick}>Men</p>
+                <p 
+                onClick={props.onMenClick}>Men</p>
                 <p onClick={props.onWomenClick}>women</p>
                 <p>Casual</p>
                 <p>brands</p>
@@ -36,11 +67,17 @@ function Nav(props){
                 <img src={cart}/>
                 <img src={user}/>
             </div>
-            <div className='toggle'>
-              <div className='bar'></div>
-              <div className='bar'></div>
-              <div className='bar'></div> 
+            <div onClick={() => {toggleDropDown(); toggleX();}} className='toggle'>
+                <div className='bar'></div>
+                <div className='bar'></div>
+               <div className='bar'></div> 
             </div>
+            <DropDown 
+        toggleDropDown ={toggleDropDown}
+        onMenClick={props.onMenClick}
+        onWomenClick={props.onWomenClick}
+        onMainClick={props.onMainClick}
+        isOpen={isDropDownOpen}/>
         </nav>
     )
 };
