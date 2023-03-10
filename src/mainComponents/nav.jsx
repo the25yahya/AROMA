06010e099102829269c7af311a2aa61abc13999c
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-03-10 15:15:54 +0100

    updated phone navigation

--- src/mainComponents/nav.jsx
+++ src/mainComponents/nav.jsx
@@ -11,16 +11,59 @@ import cart from '../imgs/cart.png'
 
 
 function DropDown(props){
+  function ScrollUp(){
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+}
+function handleMainAndScroll(){
+  ScrollUp();
+  props.onMainClick();
+  props.toggleDropDown();
+  props.toggleX();
+}
+function handleNewArrivalAndScroll(){
+  ScrollUp();
+  props.onNewArrivalClick();
+  props.toggleDropDown();
+  props.toggleX();
+}
+function handleMenAndScroll(){
+  ScrollUp();
+  props.onMenClick();
+  props.toggleDropDown();
+  props.toggleX();
+}
+function handleWomenAndScroll(){
+  ScrollUp();
+  props.onWomenClick();
+  props.toggleDropDown();
+  props.toggleX();
+}
+function handleCasualAndScroll(){
+  ScrollUp();
+  props.onCasualClick();
+  props.toggleDropDown();
+  props.toggleX();
+}
+function handleBrandsAndScroll(){
+  ScrollUp();
+  props.onBrandsClick();
+  props.toggleDropDown();
+  props.toggleX();
+}
   const dropdownClassname = `dropdown ${props.isOpen ? 'open' : 'show'}`;
   return(
     <div className={dropdownClassname}>
                 <h1>menu</h1>
-                <p onClick={props.onMainClick}>Home</p>
-                <p onClick={props.onNewArrivalClick}>new arrival</p>
-                <p onClick={props.onMenClick}>Men</p>
-                <p onClick={props.onWomenClick}>women</p>
-                <p onClick={props.onCasualClick}>Casual</p>
-                <p onClick={props.onBrandsClick}>brands</p>
+                <p onClick={handleMainAndScroll}>Home</p>
+                <p onClick={handleNewArrivalAndScroll}>new arrival</p>
+                <p onClick={handleMenAndScroll}>Men</p>
+                <p onClick={handleWomenAndScroll}>women</p>
+                <p onClick={handleCasualAndScroll}>Casual</p>
+                <p onClick={handleBrandsAndScroll}>brands</p>
     </div>
   )
 }
@@ -78,6 +121,7 @@ function Nav(props){
         onCasualClick={props.onCasualClick}    
         onNewArrivalClick={props.onNewArrivalClick}    
         toggleDropDown ={toggleDropDown}
+        toggleX={toggleX}
         onMenClick={props.onMenClick}
         onWomenClick={props.onWomenClick}
         onMainClick={props.onMainClick}
