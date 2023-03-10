2c88ed5d63002b07b8c98d5258751592c5c29258
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-03-10 23:55:29 +0100

    added hover effect to products

--- src/mainComponents/product.jsx
+++ src/mainComponents/product.jsx
@@ -9,10 +9,23 @@ import react,{ useState } from 'react'
 
 
 function Product(props){
+  const [fade, setfade] = useState(false);
+  const [imgSrc, setImgSrc] = useState(props.img1);
+  const handleMouseEnter = () => {
+    setImgSrc(props.img2);
+    setfade(true);
+  }
+  const handleMouseLeave = () => {
+    setImgSrc(props.img1);
+    setfade(false);
+  }
     return(
         <div className='product'>
           <img
-            src={props.img1}
+            className={fade ? 'fade-in' : ''}
+            src={imgSrc}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
           />
           <h2 id='name'>{props.name}</h2>
           <p id='price'>{props.price}</p>
